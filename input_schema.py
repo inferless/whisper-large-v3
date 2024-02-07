7815c505310f69150bc4e9c9fb26f335b78c9399
rbgo404 <150957746+rbgo404@users.noreply.github.com>
2024-02-07 11:09:25 +0530

    Create input_schema.py

---
+++ input_schema.py
@@ -0,0 +1,8 @@
+INPUT_SCHEMA = {
+    "audio_url": {
+        'datatype': 'STRING',
+        'required': True,
+        'shape': [1],
+        'example': ["http://thepodcastexchange.ca/s/Porsche-Macan-July-5-2018-1.mp3"]
+    }
+}
